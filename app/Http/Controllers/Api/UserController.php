58dba9fc5118c07022946c9af9ca0612086cd15d
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-01 11:47:47 +0800

    menu lagi

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -60,7 +60,7 @@ class UserController extends Controller
       $data = DB::table('menu')->where('kode_klp' , $kode)->get();
 
       if ($data) {
-        $menu_satu = DB::table('menu')->whereBetween('rowindex' , [2,4])->get();
+        $menu_satu = DB::table('menu')->whereBetween('kode_form' , ['F02','F03'])->get();
 
         $menu_dua = DB::table('menu')->where([
           ['kode_klp', '=', $kode],
