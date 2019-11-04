2389273dcc586ba5878c4ec4f3cc8e3a35c1820c
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-04 12:03:02 +0800

    relasi table

--- app/MenuForm.php
+++ app/MenuForm.php
@@ -8,4 +8,9 @@ class MenuForm extends Model
 {
     protected $table = "m_form";
     public $timestamps = false;
+
+    public function nama_form(){
+        return $this->belongsTo('App\Siswa');
+    }
+
 }
--- app/Siswa.php
+++ app/Siswa.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     protected $table = "siswa";
+
+    public function kode_form(){
+      return $this->hasOne('App\MenuForm');
+    }
+
 }
