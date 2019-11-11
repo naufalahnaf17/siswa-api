5a1805111cebf602f28d07ecada4188acf1e66fd
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-11 16:19:36 +0800

    add kode menu register

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','url_photo',
+        'name', 'email', 'password','url_photo','kode_menu',
     ];
 
     /**
