ab8a4abb52b84dc427ae7b67de6f1deb9d87d397
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 03:46:08 +0800

    exception handler finish

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,8 +1,3 @@
-<?php
-
-  $res['Error'] = "Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu";
-  $res['Message'] = "You Should Insert Your Token Into Header Option In Postman On GuzzleHttp";
-  $res['Example'] = "Bearer *insert Your Token Here  | Bearer abcdefghijklmnopqxxxx";
-  return response($res,401);
-
-?>
+Unauthorized
+Kamu Belum Memasukan Token Pada Header Request HTTP Kamu
+Contoh Penggunaan Pada Header : Authorization Isi Dengan Bearer *token kamu
