c5ef87eec44d1e7d696773561f1458d3c1926879
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 03:14:55 +0800

    add exception handler v1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,7 +50,9 @@ class Handler extends ExceptionHandler
      public function render($request, Exception $exception)
       {
           if ($exception instanceof MethodNotAllowedHttpException) {
-              return response('Salah Memakai Method' . $exception->getMessage() , 405);
+            $res['error'] = "Salah Pakai Method";
+            $res['suggest'] = $exception->getMessage();
+            return response($res);
           }
           if ($exception instanceof NotFoundHttpException) {
               return response('Token Nya Masukin Dulu Goblok' , 401);
