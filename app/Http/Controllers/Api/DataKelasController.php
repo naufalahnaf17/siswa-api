8c07f506003ced0185fae04e4ae05ca475d3463b
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-15 22:51:00 +0800

    add data kelas insert v1

--- app/Http/Controllers/Api/DataKelasController.php
+++ app/Http/Controllers/Api/DataKelasController.php
@@ -28,4 +28,45 @@ class DataKelasController extends Controller
 
     }
 
+    public function tambah(Request $request)
+    {
+
+      $validator = Validator::make($request->all(), [
+          'kode_kelas' => 'required',
+          'nama' => 'required',
+          'kode_tingkat' => 'required',
+          'kode_jur' => 'required',
+      ]);
+
+      if ($validator->fails()) {
+          return response()->json(['error' => $validator->errors()], 401);
+      }
+
+      $kode_kelas = $request->input('kode_kelas');
+      $nama = $request->input('nama');
+      $kode_lokasi = '12';
+      $kode_tingkat = $request->input('kode_tingkat');
+      $kode_pp = 'YSPTE05';
+      $kode_jur = $request->input('kode_jur');
+      $flag_aktif = '1';
+
+      $data = new DataKelas;
+      $data->kode_kelas = $kode_kelas;
+      $data->nama = $nama;
+      $data->kode_lokasi = $kode_lokasi;
+      $data->kode_tingkat = $kode_tingkat;
+      $data->kode_pp = $kode_pp;
+      $data->kode_jur = $kode_jur;
+      $data->flag_aktif = $flag_aktif;
+
+      if ($data->save()) {
+        $res['message'] = "Success Menyimpan Data";
+        return response($res);
+      }else {
+        $res['message'] = "Gagal Menyimpan Data";
+        return response($res);
+      }
+
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -62,6 +62,7 @@ Route::group(['middleware' => 'auth:api'], function() {
 
     // Crud Siswa Data Kelas
     Route::get('siswa-kelas' , 'Api\DataKelasController@index');
+    Route::post('siswa-kelas' , 'Api\DataKelasController@tambah');
 
     // Upload Image For Profile
     Route::get('file/download/{nama}' , 'Api\FileController@download');
