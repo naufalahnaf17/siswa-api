341ca9a205d2fe2c82058f2f516ada12cfe00e68
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 03:34:12 +0800

    add exception handler v6 fix 2

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,11 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            return abort(403);
+          $res['error'] = "Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu";
+          $res['message'] = "You Should Insert Your Token Into Header Option In Postman On GuzzleHttp";
+          $res['format-input'] = "Bearer *insert Your Token Here";
+          $res['example'] = "Bearer abcdefghijklmnopqxxxx";
+          return response($res,403);
         }
     }
 }
