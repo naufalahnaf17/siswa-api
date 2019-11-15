197608e42e7ebab06fcd746140aa5a13e881b89f
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 03:38:56 +0800

    exception handler finish

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,1 +1,4 @@
-Anjing
+Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu <br>
+You Should Insert Your Token Into Header Option In Postman On GuzzleHttp <br>
+Bearer *insert Your Token Here <br>
+Bearer abcdefghijklmnopqxxxx"; <br>
