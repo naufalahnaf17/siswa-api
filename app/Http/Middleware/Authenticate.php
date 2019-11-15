68adb23a5e498d38f0c7db0b6f3da635532461d5
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 02:12:02 +0800

    add middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (!$request->expectsJson()) {
-            abort(404 , 'Akun Tidak Ditemukan');
+            return response($this->getStatusCode());
         }
     }
 }
