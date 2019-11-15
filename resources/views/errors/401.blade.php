382359c4f0ac439a4522f31e345a402564444e99
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 03:39:44 +0800

    exception handler remove break line finish

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,4 +1,4 @@
-Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu <br>
-You Should Insert Your Token Into Header Option In Postman On GuzzleHttp <br>
-Bearer *insert Your Token Here <br>
-Bearer abcdefghijklmnopqxxxx"; <br>
+Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu
+You Should Insert Your Token Into Header Option In Postman On GuzzleHttp
+Bearer *insert Your Token Here
+Bearer abcdefghijklmnopqxxxx";
