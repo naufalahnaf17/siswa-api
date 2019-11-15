c7ae09d3f942d31595e12a1b32d640e10c3501d3
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-16 03:43:06 +0800

    exception handler remove break line finish

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,4 +1,8 @@
-Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu
-You Should Insert Your Token Into Header Option In Postman On GuzzleHttp
-Bearer *insert Your Token Here
-Bearer abcdefghijklmnopqxxxx";
+<?php
+
+  $res['Error'] = "Token Belum Dimasukan , Harap Masukan Token Pada Header Postman / Http Request Mu";
+  $res['Message'] = "You Should Insert Your Token Into Header Option In Postman On GuzzleHttp";
+  $res['Example'] = "Bearer *insert Your Token Here  | Bearer abcdefghijklmnopqxxxx";
+  return response($res,401);
+
+?>
