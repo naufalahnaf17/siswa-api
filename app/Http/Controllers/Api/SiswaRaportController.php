f62207710b88b6be53d69d27e0cfd50fc9b63c26
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-19 16:47:33 +0800

    add more fixed data

--- app/Http/Controllers/Api/SiswaRaportController.php
+++ app/Http/Controllers/Api/SiswaRaportController.php
@@ -78,9 +78,7 @@ class SiswaRaportController extends Controller
     $input['kode_lokasi'] = '12';
     $input['kode_pp'] = 'YSPTE05';
     $data = SiswaRaport::where('nis',$nis_edit)->where('kode_pp','YSPTE05')->update(
-      ['kode_sem' => $input['kode_sem']],
-      ['kode_kelas' => $input['kode_kelas']],
-      ['nis' => $input['nis']]
+      ['nis' => $input['nis'] , 'kode_kelas' => $input['kode_kelas']]
     );
 
     if ($data) {
