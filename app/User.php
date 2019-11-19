c3572ae5590acc98866658ef0a1422183caefa6f
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-19 12:54:52 +0800

    add more option

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','url_photo','kode_menu',
+        'name', 'email', 'password','url_photo','kode_menu','nis',
     ];
 
     /**
