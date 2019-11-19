cdfdada2e29e709eba66b0674db0b742ec7999a0
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-19 15:19:51 +0800

    ad

--- app/Http/Controllers/Api/SiswaPrestasiController.php
+++ app/Http/Controllers/Api/SiswaPrestasiController.php
@@ -85,7 +85,7 @@ class SiswaPrestasiController extends Controller
     $input = $request->all();
     $input['kode_lokasi'] = '12';
     $input['kode_pp'] = 'YSPTE05';
-    $data = SiswaPrestasi::where('nis',$nis_edit)->where('kode_pp' , 'YSPTE05')->where('kode_pp' , '12')->update('keterangan' , $input['keterangan']);
+    $data = SiswaPrestasi::where('nis',$nis_edit)->where('kode_pp' , 'YSPTE05')->where('kode_pp' , '12')->update('keterangan' => $input['keterangan']);
 
     if ($data) {
       $res['message'] = "Success Mengubah Data";
