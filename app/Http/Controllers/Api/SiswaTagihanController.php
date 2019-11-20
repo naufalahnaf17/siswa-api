c9f7fd991b06e3fe4d07a15e562c3797cdd6700f
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-20 16:29:27 +0800

    add tagihan

--- app/Http/Controllers/Api/SiswaTagihanController.php
+++ app/Http/Controllers/Api/SiswaTagihanController.php
@@ -11,12 +11,10 @@ class SiswaTagihanController extends Controller
     public function index($nis)
     {
 
-      $nis_siswa = $nis;
-
       $tagihan = DB::table('sis_siswa')
       ->join('sis_siswa_tarif', function ($join) {
           $join->on('sis_siswa.nis', '=', 'sis_siswa_tarif.nis')
-               ->where('sis_siswa_tarif' , '=' , $nis_siswa);
+               ->where('sis_siswa' , '=' , $nis);
       })
       ->get();
 
