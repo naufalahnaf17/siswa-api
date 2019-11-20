fa377597cd91e28d6d3f1b7dd77e5c92df22105a
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-20 11:08:44 +0800

    add jam fixed

--- app/Http/Controllers/Api/JadwalController.php
+++ app/Http/Controllers/Api/JadwalController.php
@@ -21,10 +21,11 @@ class JadwalController extends Controller
       if (count($data) > 0 ) {
 
         $kelas_11 = DB::table('sis_jadwal')
-                    ->join('sis_slot', 'sis_jadwal.kode_slot', '=', 'sis_slot.kode_slot')
-                    ->where('kode_lokasi' , '12')
-                    ->select('sis_jadwal.*', 'sis_slot.nama')
-                    ->get();
+        ->join('sis_slot', function ($join) {
+            $join->on('sis_jadwal.kode_slot', '=', 'sis_slot.kode_slot')
+                 ->where('sis_jadwal.kode_pp', '=', 'YSPTE05');
+        })
+        ->get();
 
         $kelas_2tkj = Jadwal::where([
           ['kode_lokasi', '=', '12'],
