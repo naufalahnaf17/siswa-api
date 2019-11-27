52663a03c25258cd908c68e8454282f585d27106
naufalahnaf17 <naufalahnaf37@gmail.com>
2019-11-27 11:05:44 +0800

    salman gaje

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
 
     public function login()
     {
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+        if(Auth::attempt(['nis' => request('nis'), 'password' => request('password')])) {
             $user = Auth::user();
             $success['token'] =  $user->createToken('Breakpoin Appliction')->accessToken;
             return response()->json(['success' => $success], $this->successStatus);
